d35749e1fff9724a5a4cadf65455601f30f5e5fd
Jolanda Wilfer <jolanda.wilfer@icloud.com>
2020-08-18 17:32:39 +0200

    initializer

---
+++ src/main/java/de/matejo/integrationhttp/IntegrationHttpApplication.java
@@ -0,0 +1,13 @@
+package de.matejo.integrationhttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationHttpApplication.class, args);
+	}
+
+}
